cb8729590ac0c12746ac9cf0897bf81456ba1748
imjamesruiz <jamesruiz610@gmail.com>
2025-05-08 21:55:10 -0700

    Plant theme update: new branding, UI, and features

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to Ping</h1>
+        <p>A Cozy, Plant-Themed Habit Tracker</p>
+      </header>
+    </div>
+  );
+}
+
+export default App; 
